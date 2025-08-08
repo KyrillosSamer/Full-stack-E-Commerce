ee242e7274b47ee0782d38794379347767ba2abb
KyrillosSamer <kyrillossamer@example.com>
2025-08-08 08:40:15 +0200

    Add some Feature

--- src/Components/Home/CategorySlider/CategorySlider.jsx
+++ src/Components/Home/CategorySlider/CategorySlider.jsx
@@ -20,47 +20,47 @@ export default function CategorySlider() {
     getCategories();
   }, []);
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 7,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1000,
-  responsive: [
-    {
-      breakpoint: 1024, 
-      settings: {
-        slidesToShow: 5,
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 5000,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024, 
+        settings: {
+          slidesToShow: 5,
+        },
       },
-    },
-    {
-      breakpoint: 768, 
-      settings: {
-        slidesToShow: 4,
+      {
+        breakpoint: 768, 
+        settings: {
+          slidesToShow: 4,
+        },
       },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        },
       },
-    },
-  ],
-};
-
+    ],
+  };
 
   return (
     <div className='w-4/5 mx-auto my-8'> 
       <h2 className='text-xl font-semibold mb-4'>Shop Popular Categories</h2>
       <Slider {...settings}> 
         {categories.map((category) => (
-          <div key={category._id} className='text-center px-2'>
+          <div key={category._id} className='text-center '>
             <img
               src={category.image}
               alt={category.name}
-              className='w-full h-[150px] object-fit '
+              className='w-full h-[165px] object-fit'
             />
             <p className='mt-2 text-sm'>{category.name}</p>
           </div>
